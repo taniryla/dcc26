e75023ec910e4f8494bde134cfd49d03032123ba
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-16 10:48:06 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+/*-----------------------------------------------------------------
+Challenge: 26-toCamelCase
+Difficulty:  Intermediate
+Prompt:
+- Write a function called toCamelCase that accepts a single string as argument.
+- The toCamelCase function should return the string as camel-cased, removing each _ or - characters and capitalizing the character following the _ or -.
+Hints:
+- This is a great challenge for using regular expressions combined with the String.replace method.
+Examples:
+toCamelCase( 'wdi-rocks' ) // => 'wdiRocks'
+toCamelCase( 'banana_Turkey_potato' ) // => 'bananaTurkeyPotato'
+toCamelCase( 'Mama-mia' ) // => 'MamaMia'
+toCamelCase( 'A_b_c' ) // => 'ABC'
+-----------------------------------------------------------------*/
+// Your solution for 26-toCamelCase here:
+</div>
+`;
